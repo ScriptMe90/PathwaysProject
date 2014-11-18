348d93304860062e5f9c4dac55f0112f2f654a75
Joshua <josh.ladon@yahoo.com>
2014-11-18 16:46:44 -0600

    Main Driver
    The driver may have to be modified some but the Purpose statement is
    solid.

---
+++ PathwayDriver.cpp
@@ -0,0 +1,16 @@
+/********************************************************************
+Name:       Justin Brumley, Cory Feliciano, Joshua Ford.
+Class:      CS 355
+Instructor: Dr. Jenkins.
+Project:    Sorting Pathways.
+Purpose:    Justin sucks donkey cock.
+********************************************************************/
+
+#include <iostream>
+useing namespace std;
+
+int main(){
+    Pandemic P;
+    
+    return P.Run;
+}
